ce0c77eb4a4709d37b0f84f54c7e7433a3333aac
Johnny Perez <johnny.perez@pcc.edu>
2020-04-21 15:35:04 -0700

    added main method

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,12 @@
 package edu.pcc.cis.helloworld;
 
+/**
+ * Simple project to say hello world
+ * Author: Johnny Perez
+ * Version: 04.21.2020
+ */
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
 }
